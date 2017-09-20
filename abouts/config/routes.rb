59a11ba618e485eca96035347fc090c7c2803048
umuru-y <s16t251@stu.kagawa-u.ac.jp>
2017-09-20 13:55:49 +0900

    aboutsコントローラー作成

---
+++ abouts/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'abouts/help'
+
+  get 'abouts/test'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
